4c4470421135d78beb846c4c8f7b699c1da4bbae
hyuntak03 <hyuntak03.lee@gmail.com>
2018-08-12 20:36:07 +0900

    modify get.js

--- src/services/get.js
+++ src/services/get.js
@@ -2,16 +2,16 @@ import axios from 'axios';
 
 let axiosConfig  = {
     headers: {
-        'Content-Type': "applicantion/json: charset=UTF-B",
-        "Access-Controlo-Allow_Origin": "*",
+        'Content-Type': "applicantion/json: charset=UTF-8",
+        "Access-Control-Allow-Origin": "*",
     }
 };
 
 export function getMsg(){
- //   return axios.get('http://localhost:8080/',{
-     return axios.get('https://jsonplaceholder.typicode.com/todos/1', {
+    return axios.get('http://localhost:8080/msg/',{
+    //  return axios.get('https://jsonplaceholder.typicode.com/todos/1', {
         params: {
-
+            name: 'Hyuntak',
         },
         axiosConfig
     });
